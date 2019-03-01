2fb6cc6a481edafe54ff4024a9b2c16ecbd28ad6
Siarhei Mitasau <darkenRahl1893@gmail.com>
2019-03-01 19:39:13 +0300

    Added implementation for love-triangles func

--- src/index.js
+++ src/index.js
@@ -4,4 +4,15 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let lovers = 0;
+  preferences.unshift(0);
+  for (let i = 1; i < preferences.length; i++) {
+    let a = preferences[i];
+    let b = preferences[a];
+    let c = preferences[b];
+    if (c == i) {
+      lovers++;
+    }
+  }
+  return parseInt(lovers / 3);
 };
